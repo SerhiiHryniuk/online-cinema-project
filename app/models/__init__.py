3c8b6e4f0384b2c7aa5788a8c13bf5c9d6846d76
Vladyslav <saccharcrpt@gmail.com>
2026-07-03 12:54:30 +0200

    feat: add user, user group and user profile models

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.accounts import (
+    GenderEnum,
+    User,
+    UserGroup,
+    UserGroupEnum,
+    UserProfile,
+)
+
+__all__ = [
+    "GenderEnum",
+    "User",
+    "UserGroup",
+    "UserGroupEnum",
+    "UserProfile",
+]
