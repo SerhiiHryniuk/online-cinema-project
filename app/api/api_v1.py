bf10e168112e8cba304e6c90ccc2d3ab7eda2754
artradkevych <artemradckevich@gmail.com>
2026-07-04 16:34:32 +0300

    feat: add accounts router

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
+from app.routers.accounts import router as accounts_router
+
 api_router = APIRouter()
+
+api_router.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
