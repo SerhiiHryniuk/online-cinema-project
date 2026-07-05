eb4b3b508a9fa6e174f023b88945c2e86740d641
Vladyslav <saccharcrpt@gmail.com>
2026-07-05 20:17:50 +0200

    feat: add movie like/dislike endpoints

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.routers.accounts import router as accounts_router
+from app.routers.likes import router as likes_router
 from app.routers.movies import router as movies_router
 from app.routers.profiles import router as profiles_router
 
@@ -9,3 +10,4 @@ api_router = APIRouter()
 api_router.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
 api_router.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
 api_router.include_router(movies_router, prefix="/movies", tags=["movies"])
+api_router.include_router(likes_router, prefix="/movies", tags=["likes"])
