ddc930065ef97a7e90bbe3a2d04e824b8dd0ceb0
artradkevych <artemradckevich@gmail.com>
2026-07-05 09:55:36 +0300

    feat: add schemas for password reset/change

--- app/schemas/accounts.py
+++ app/schemas/accounts.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 from typing import Annotated
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field, AfterValidator
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    EmailStr,
+    Field,
+    AfterValidator,
+    model_validator
+)
 
 
 def validate_password_strength(password: str) -> str:
@@ -28,9 +35,19 @@ ComplexPassword = Annotated[
 ]
 
 
-class UserRegistrationRequestSchema(BaseModel):
-    email: EmailStr
+class PasswordMatchMixin(BaseModel):
     password: ComplexPassword
+    password_confirm: str = Field(min_length=8, max_length=128)
+
+    @model_validator(mode="after")
+    def verify_password_match(self) -> "PasswordMatchMixin":
+        if self.password != self.password_confirm:
+            raise ValueError("The two passwords do not match.")
+        return self
+
+
+class UserRegistrationRequestSchema(PasswordMatchMixin):
+    email: EmailStr
 
 
 class UserRegistrationResponseSchema(BaseModel):
@@ -67,3 +84,15 @@ class TokenResponseSchema(BaseModel):
 
 class TokenRefreshSchema(BaseModel):
     refresh_token: str
+
+
+class PasswordChangeRequestSchema(PasswordMatchMixin):
+    old_password: str = Field(min_length=8, max_length=128)
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(PasswordMatchMixin):
+    token: str
