36713616d31d73e5c92978ecbb6c52a600647041
smykroman-dot <smykroman@gmail.com>
2026-07-07 08:32:37 +0300

    feat: cart-items-crud
    * feat: add Pydantic schemas for cart
    
    * feat: implement CRUD functions for cart
    
    * feat: add CartNotFound and MovieNotFound custom exceptions
    
    * feat: implement cart API endpoints, admin views, and movie deletion protection
    
    * test: add tests for cart operations
    
    * feat: implement shopping cart functionality
    
    * feat: implement shopping cart functionality
    
    * fix: ensure movie data is loaded in add_movie_to_cart
    
    ---------
    
    Co-authored-by: Roman Smyk <smyk_roman@ukr.net>

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -13,11 +13,13 @@ from app.routers.notifications import router as notifications_router
 from app.routers.profiles import router as profiles_router
 from app.routers.ratings import router as ratings_router
 from app.routers.stars import router as stars_router
+from app.routers.carts import router as carts_router
 
 api_router = APIRouter()
 
 api_router.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
 api_router.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
+api_router.include_router(carts_router, prefix="/carts", tags=["carts"])
 api_router.include_router(
     favorites_router, prefix="/movies", tags=["favorites"]
 )
